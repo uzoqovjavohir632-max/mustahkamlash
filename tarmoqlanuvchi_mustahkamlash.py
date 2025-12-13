026016913ac7103feae469006d0898a82c79eb51
javohir <uzoqovjavohir632@gmail.com>
2025-12-13 22:46:09 +0500

    first commit

---
+++ tarmoqlanuvchi_mustahkamlash.py
@@ -0,0 +1,19 @@
+# talabalar = ["anvar", "jahongir", "muhriddin" , "shohrux" ]
+# for talaba in talabalar :
+#     print ("xush kelipsiz " , talaba ,"'BUXORO DAVLAT TEXNIKA UNIVERSITETIGA'")
+
+
+# a= int (input (" a sonni kiriting :"))
+# if a% 2 == 0 :
+#     print ( " juft son")
+# else : 
+#     print ("toq son ")    
+
+
+
+
+parol = input (" parolni kiriting :")
+if len (parol) >= 8 :
+    print ("kuchli parol")
+else: 
+    print(" kuchli emas")
