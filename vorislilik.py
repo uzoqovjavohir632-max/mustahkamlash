50d703a6ad5359bfeab247728e84c6f8e3239d5d
javohir <uzoqovjavohir632@gmail.com>
2026-01-04 01:39:34 +0500

    davomi bor

--- vorislilik.py
+++ vorislilik.py
@@ -91,34 +91,126 @@
 # Xodim klassi (ism, maosh). Dasturchi, Menejer, Dizayner klasslarini yarating.
 # Har birida ish_qil() metodi bo'lsin. 
 
-class Xodim():
-    def __init__( self, ism, maosh ) :
-        self.ism = ism 
-        self.maosh = maosh
-    def malumot (self):
-       print (f" xodimning ismi {self.ism } , maoshi esa {self. maosh }")
-class Dasturchi(Xodim) :
-   def ish_qil(self ):
-      print ("dasturchi kod yozmoqda")
-class Menejer(Xodim) :
-    def ish_qil(self):
-      print("menejer ishlamoqda ")   
-class Dizayner(Xodim):
-    def ish_qil(self):
-       print ("diazyner dizayn yaratmoqda ")
-def  kiritish(klass):
-     ism = input("xodim ismini kiriting ")
-     maosh = int(input("xodim oladigan maoshni kiriting" ))
-     return klass ( ism , maosh)
-print(" 1- dasturchi ", " 2- menejer " , " 3- dizayner ")
-tanlov = input ("tanlang : ")
-if tanlov == "1" :
-    t=kiritish(Dasturchi)
-if tanlov == "2" :
-    t=kiritish(Menejer)
-if tanlov == "3" :
-    t=kiritish(Dizayner)    
+# class Xodim():
+#     def __init__( self, ism, maosh ) :
+#         self.ism = ism 
+#         self.maosh = maosh
+#     def malumot (self):
+#        print (f" xodimning ismi {self.ism } , maoshi esa {self. maosh } so'm ")
+# class Dasturchi(Xodim) :
+#    def ish_qil(self ):
+#       print ("dasturchi kod yozmoqda")
+# class Menejer(Xodim) :
+#     def ish_qil(self):
+#       print("menejer ishlamoqda ")   
+# class Dizayner(Xodim):
+#     def ish_qil(self):
+#        print ("diazyner dizayn yaratmoqda ")
+# def  kiritish(klass):
+#      ism = input("xodim ismini kiriting:  ")
+#      maosh = int(input("xodim oladigan maoshni kiriting ( so'mda ): " ))
+#      return klass ( ism , maosh)
+# print(" 1- dasturchi ", " 2- menejer " , " 3- dizayner ")
+# tanlov = int(input ("tanlang : "))
+# if tanlov == 1:
+#     t=kiritish(Dasturchi)
+# elif tanlov == 2 :
+#     t=kiritish(Menejer)
+# elif tanlov == 3 :
+#     t=kiritish(Dizayner) 
 
-t.malumot()
-t.ish_qil()
+# else :
+#     print  ( "matn kiritmang yoki 4 dan kichik butun son kiriting ")   
+#     exit()
+# t.malumot()
+# t.ish_qil()
 
+
+
+# 4- misol 
+# Shakl klassi yarating. Undan Doira, Tortburchak, Uchburchak hosil qiling. Har birida 
+# yuza() va perimetr() metodlari bo'lsin. 
+class Shakl():
+    def __init__(self, nom):
+        self.nom = nom
+
+    def malumot(self):
+        print("Shakl nomi:", self.nom)
+
+
+class Doira(Shakl):
+    def __init__(self, nom, r):
+        self.nom = nom
+        self.r = r
+
+    def yuza(self):
+        print("Yuza:", 3.14 * self.r * self.r)
+
+    def perimetr(self):
+        print("Perimetr:", 2 * 3.14 * self.r)
+
+
+class Tortburchak(Shakl):
+    def __init__(self, nom, a, b):
+        self.nom = nom
+        self.a = a
+        self.b = b
+
+    def yuza(self):
+        print("Yuza:", self.a * self.b)
+
+    def perimetr(self):
+        print("Perimetr:", 2 * (self.a + self.b))
+
+
+class Uchburchak(Shakl):
+    def __init__(self, nom, a, b, c):
+        self.nom = nom
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def yuza(self):
+        print("Yuza:", (self.a * self.b) / 2)
+
+    def perimetr(self):
+        print("Perimetr:", self.a + self.b + self.c)
+
+
+def kiritish(klass):
+    nom = input("Shakl nomini kiriting: ")
+
+    if klass == Doira:
+        r = int(input("Radiusni kiriting: "))
+        return klass(nom, r)
+
+    if klass == Tortburchak:
+        a = int(input("a ni kiriting: "))
+        b = int(input("b ni kiriting: "))
+        return klass(nom, a, b)
+
+    if klass == Uchburchak:
+        a = int(input("a ni kiriting: "))
+        b = int(input("b ni kiriting: "))
+        c = int(input("c ni kiriting: "))
+        return klass(nom, a, b, c)
+
+
+print("1- Doira", "2- To‘rtburchak", "3- Uchburchak")
+tanlov = int(input("Tanlang: "))
+
+if tanlov == 1:
+    s = kiritish(Doira)
+elif tanlov == 2:
+    s = kiritish(Tortburchak)
+elif tanlov == 3:
+    s = kiritish(Uchburchak)
+else:
+    print("Noto‘g‘ri tanlov!")
+    exit()
+
+s.malumot()
+s.yuza()
+s.perimetr()
+
+                 
